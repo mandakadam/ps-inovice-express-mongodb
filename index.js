b98638f370ffe2e24381140e68c6416332c59e00
mandakadam1 <mandakadam@credenceanalalytics.com>
2021-01-08 19:25:36 +0530

    headers added

--- index.js
+++ index.js
@@ -33,10 +33,9 @@ app.get('/', (req, res)=>{
 })
 
 app.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin','*');
-    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
-    res.header('Access-Control-Allow-Credentials', true);
-    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, PATCH');
+    res.header('Access-Control-Allow-Origin: *');
+    res.header('Access-Control-Allow-Headers: Content-Type');
+    res.header('Access-Control-Allow-Methods: POST')
     next();
 });
 
