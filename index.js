0abb0f866f53ab30027168f8f929c0aa83f8c085
mandakadam1 <mandakadam@credenceanalalytics.com>
2021-01-08 19:21:48 +0530

    headers added

--- index.js
+++ index.js
@@ -32,6 +32,14 @@ app.get('/', (req, res)=>{
     res.send('Test Data')
 })
 
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin','*');
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+    res.header('Access-Control-Allow-Credentials', true);
+    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, PATCH');
+    next();
+});
+
 //User Mongoose DB Connection
 mongoose
 .connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
