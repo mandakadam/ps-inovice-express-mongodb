f17c4a6fdff66539c95b84836543ecb7e20c789f
manda <mandakadam@credenceanalytics.com>
2022-10-05 19:29:26 +0530

    cors header added

--- index.js
+++ index.js
@@ -33,8 +33,8 @@ app.get('/', (req, res)=>{
 })
 
 app.use((req, res, next) => {
-    // res.header('Access-Control-Allow-Origin: *');
-    // res.header('Access-Control-Allow-Headers: Content-Type');
+    res.header('Access-Control-Allow-Origin: *');
+    res.header('Access-Control-Allow-Headers: Content-Type');
     res.header('Access-Control-Allow-Methods: POST')
     next();
 });
